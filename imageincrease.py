d4df77d39050f2793b868d23c690bb1661ad2aa7
Sergey Grigoryan <theperfectcodder@gmail.com>
2022-07-15 14:54:10 +0400

    image incrased changes

--- imageincrease.py
+++ imageincrease.py
@@ -1,6 +1,15 @@
 from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 size = 7680, 7680
-im = Image.open("output/2022-06-08-example-prompts/girl-with-blonde-hair-by-picasso.jpg")
-im_resized = im.resize(size, Image.ANTIALIAS)
-im_resized.save("sea.jpg", dpi=(1200, 1200))
+output = 'output/'
+dirName = '2022-06-10-example-prompts'
+myPath = output + dirName
+files = [f for f in listdir(myPath) if isfile(join(myPath, f))]
+
+for file in files:
+    im = Image.open(join(myPath, file))
+    im_resized = im.resize(size, Image.ANTIALIAS)
+    im_resized.save(f"increased/{file}", dpi=(1920, 1920))
+
